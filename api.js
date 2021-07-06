8593a8d5baf0ca05103a2e501c9ebc0e457c5de3
Timothy Merritt <timmybytes@gmail.com>
2021-07-06 10:42:57 -0700

    chore: Experiment with different sports APIs --WIP

--- api.js
+++ api.js
@@ -6,11 +6,27 @@ const SPORTS_IDs = [];
 const term = 'all_sports.php';
 
 // Fetch data
-fetch(`https://www.thesportsdb.com/api/v1/json/1/${term}`).then(res =>
-  res.json().then(data => {
-    // Save sport names and associated ids as objects to SPORTS_IDs array
-    data.sports.map(sport => {
-      console.log({ name: sport.strSport, id: sport.idSport });
-    });
+// fetch(`https://www.thesportsdb.com/api/v1/json/1/${term}`).then(res =>
+//   res.json().then(data => {
+//     // Save sport names and associated ids as objects to SPORTS_IDs array
+//     data.sports.map(sport => {
+//       console.log({ name: sport.strSport, id: sport.idSport });
+//     });
+//   })
+// );
+
+fetch(
+  'https://football-pro.p.rapidapi.com/api/v2.0/livescores?tz=PST&include=localTeam%2CvisitorTeam',
+  {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'football-pro.p.rapidapi.com',
+    },
+  }
+)
+  .then(response => {
+    console.log(response);
   })
-);
+  .catch(err => {
+    console.error(err);
+  });
