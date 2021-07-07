19b30584b2b5bdcc7556e4c4f746c878a4cf2a4c
Timothy Merritt <timmybytes@gmail.com>
2021-07-07 15:02:08 -0700

    chore: Clean up API info

--- api.js
+++ api.js
@@ -1,20 +1,5 @@
 const fetch = require('node-fetch');
 
-// Sports Codes
-const SPORTS_IDs = [];
-
-const term = 'all_sports.php';
-
-// Fetch data
-// fetch(`https://www.thesportsdb.com/api/v1/json/1/${term}`).then(res =>
-//   res.json().then(data => {
-//     // Save sport names and associated ids as objects to SPORTS_IDs array
-//     data.sports.map(sport => {
-//       console.log({ name: sport.strSport, id: sport.idSport });
-//     });
-//   })
-// );
-
 fetch(
   'https://football-pro.p.rapidapi.com/api/v2.0/livescores?tz=PST&include=localTeam%2CvisitorTeam',
   {
